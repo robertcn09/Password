fa6d4eb4b09c1dda76379831f223479565fb81c0
Robert Newton <33005863+robertcn09@users.noreply.github.com>
2017-10-25 19:14:28 -0400

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,12 @@
+print ('Hello what is your name?')
+name = (input())
+if name == 'Robert':
+    print('Hi, Robert')
+elif name:
+    print('Error')
+print ('age=')
+age = (input())
+if age == 27:
+    print('You have access')
+elif age:
+    print('You are not Robert')
